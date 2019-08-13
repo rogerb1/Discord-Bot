e980d4529fdf3be7cbfaa6b6cbfd07472c15766a
rogerdavid2 <rogerb7@uw.edu>
2019-08-13 16:51:59 -0700

    Add bot command

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("This works");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = "NjEwOTU1MjQxMjQ4MjYwMTIz.XVM4mg.9VljxxQiQ2fVqmMYL3MF7byEaMM";
+
+bot.on('message', function (message) {
+    if (message.content == 'Hello') {
+        message.reply("  Hello how are you? ");
+    }
+});
+
+
+bot.login(TOKEN);
